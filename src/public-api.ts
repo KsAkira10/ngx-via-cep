124047629fe93454a26f9771614ea68800d8bb98
Lucas Akira Uehara <akirauehara@hotmail.com>
2019-03-21 11:44:53 -0300

    create index file

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-via-cep
  */
 
-export * from './lib/ngx-via-cep.service';
-export * from './lib/ngx-via-cep.component';
-export * from './lib/ngx-via-cep.module';
+export * from './index';
